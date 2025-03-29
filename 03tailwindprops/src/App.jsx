9b597fc91b266a6783dfe6912a03a533c498ab77
Himani Jangid <himanijangid07@gmail.com>
2025-03-29 17:36:26 +0530

    tailwind css

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black rounded-xl p-4 mb-4'>Tailwind test</h1>
+      <Card/>
+      <Card/>
+    </>
+  )
+}
+
+export default App
