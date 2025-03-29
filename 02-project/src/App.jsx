767b68f897d896c92ecb0bccecdca0c23d6b3764
Himani Jangid <himanijangid07@gmail.com>
2025-03-29 16:50:21 +0530

    hooks in react

---
+++ 02-project/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(15)
+
+  // let counter = 5
+
+  const addValue = () => {
+    console.log('clicked', counter)
+    // counter = counter + 1
+    setCounter(counter + 1)
+  }
+
+  const removeValue = () => {
+    if(counter > 0) {
+      setCounter(counter - 1)
+    } else {
+      alert(`can't decrease the counter less than 0`)
+    }
+  }
+
+  return (
+    <>
+      <h1>Himani Jangid</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>Add Value {counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove Value{counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
