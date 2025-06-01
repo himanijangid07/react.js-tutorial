295747c563c2ba4574eaa46edfd6e872d5283552
Himani Jangid <himanijangid07@gmail.com>
2025-06-01 18:53:54 +0530

    interview discussion

---
+++ interview-disc/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [value, setValue] = useState(1)
+  // const [multipliedValue, setMultipliedValue] = useState(1)
+  let multipliedValue = value * 5
+
+  const multiplyByFive = () => {
+    // setMultipliedValue(value * 5)
+    setValue(value + 1)
+  }
+
+  return (
+    <>
+      <h1>Main value: {value}</h1>
+      <button onClick={multiplyByFive}>Click to multiply by 5</button>
+      <h2>Multiplied value: {multipliedValue}</h2>
+    </>
+  )
+}
+
+export default App
